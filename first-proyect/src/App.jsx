1a09411a2fe8705365fe9f77e97c14a834b657df
Bermelio <facundoperalta258@gmail.com>
2025-01-16 20:48:40 -0300

    primera entrega casi bien

---
+++ first-proyect/src/App.jsx
@@ -0,0 +1,14 @@
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import NavBar from './components/NavBar/NavBar'
+
+function App() {
+
+  return (
+    <>
+      <NavBar/>
+      <ItemListContainer grettins={'Welcome back my old friend'}/>
+    </>
+  )
+}
+
+export default App
